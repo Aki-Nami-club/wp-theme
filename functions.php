0699d5cd0b77aa505e00cef495d1cfcb0251b8f3
Aki-Nami-club <dima.radioshop@gmail.com>
2026-04-26 02:24:35 +0300

    Create initial VibeMag FSE theme scaffold

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * VibeMag theme bootstrap file.
+ *
+ * @package VibeMag
+ */
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+require_once get_theme_file_path('inc/setup.php');
+require_once get_theme_file_path('inc/enqueue.php');
